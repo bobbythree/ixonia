784cd28a0c861580cb85f8fcf03c44c36c0e8e26
bobbythree <mynameisbobbylewis@hotmail.com>
2024-04-22 13:34:48 -0500

    create player object module. assign ititial values for hp, gp, weap, inv.

--- main.js
+++ main.js
@@ -1,19 +1,37 @@
+import { player } from './modules/player.js';
 import { backgrounds } from './modules/backgrounds.js';
 import { narrations } from './modules/narrations.js'
 import { buttons } from './modules/buttons.js'
 import * as dialogs from './modules/dialogs.js'
 
+//selectors
 const screen = document.getElementById('screen');
 const text = document.getElementById('text');
 const dialogBox = document.getElementById('dialog');
 const narrationBox = document.getElementById('narration');
+const hpText = document.getElementById('hpText');
+const gpText = document.getElementById('gpText');
+const weaponsText = document.getElementById('weaponsText');
+const invText = document.getElementById('invText');
 
 //start game
 window.addEventListener('load', () => {
+  startGame();
+});
+
+function startGame() {
   setBackground('title');
   createNarration('title');
   createButtons('title');
-});
+  player.hp = 50;
+  player.gp = 100;
+  player.weapons = ['Wooden Sword'];
+  player.inv = [];
+  hpText.innerText = player.hp;
+  gpText.innerText = player.gp;
+  weaponsText.innerText = player.weapons;
+  invText.innerText = player.inv;
+}
 
 function clickHandler(route, buttonType) {  
   switch(buttonType) {
